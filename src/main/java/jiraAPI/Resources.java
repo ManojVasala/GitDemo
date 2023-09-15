aa22cb4d0b1e893780d39c009f821ca040c0083e
manoj <manoj.vasala200@gmail.com>
2023-09-15 06:36:51 +0530

    american guy commit

--- src/main/java/jiraAPI/Resources.java
+++ src/main/java/jiraAPI/Resources.java
@@ -5,6 +5,14 @@ public class Resources {
 	public static String createComment()
 	{
 		String res="/rest/api/2/issue/10036/comment";
+		//test1
+				//test2
+				//test3
+				//test4
+		System.out.println("PostJira4");
+		System.out.println("PostJira5");
+		System.out.println("PostJira6");
+		System.out.println("PostJira7");
 		return res;
 		
 	}
