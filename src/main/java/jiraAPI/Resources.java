7857fb6f14392f82faed91912af486b6bbc459d6
manoj <manoj.vasala200@gmail.com>
2023-09-15 06:47:21 +0530

    from git demo

--- src/main/java/jiraAPI/Resources.java
+++ src/main/java/jiraAPI/Resources.java
@@ -5,6 +5,8 @@ public class Resources {
 	public static String createComment()
 	{
 		String res="/rest/api/2/issue/10036/comment";
+		System.out.println("Post jira2");
+		System.out.println("Post jira3");
 		return res;
 		
 	}
