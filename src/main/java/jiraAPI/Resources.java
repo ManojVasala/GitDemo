2bcd3527d825409461a202774ac58b422eb9588c
manoj <manoj.vasala200@gmail.com>
2023-09-15 07:41:22 +0530

    first commit Git demo

--- src/main/java/jiraAPI/Resources.java
+++ src/main/java/jiraAPI/Resources.java
@@ -7,6 +7,7 @@ public class Resources {
 		String res="/rest/api/2/issue/10036/comment";
 		System.out.println("Post jira2");
 		System.out.println("Post jira3");
+System.out.println("Post jira3");
 		return res;
 		
 	}
