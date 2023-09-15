9c051c3129adeb73b9c2395350b7af5ba4be6fb5
manoj <manoj.vasala200@gmail.com>
2023-09-15 06:17:57 +0530

    this is commit from asian timezone guy

--- src/main/java/jiraAPI/Resources.java
+++ src/main/java/jiraAPI/Resources.java
@@ -5,6 +5,8 @@ public class Resources {
 	public static String createComment()
 	{
 		String res="/rest/api/2/issue/10036/comment";
+		System.out.println("PostJir2");
+		System.out.println("PostJira3");
 		return res;
 		
 	}
