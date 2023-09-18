b3f15b19d3763923daf78ff45a9f4a81dc131fce
manoj <manoj.vasala200@gmail.com>
2023-09-18 08:15:36 +0530

    final commit from gitx

---
+++ src/main/java/jiraAPI/Resources - Copy.java
@@ -0,0 +1,22 @@
+package jiraAPI;
+
+public class Resources {
+
+	public static String createComment()
+	{
+		String res="/rest/api/2/issue/10036/comment";
+		System.out.println("Post jira2");
+		System.out.println("Post jira3");
+System.out.println("Post jira3");
+System.out.println("Post jira4");
+System.out.println("Post jira4");
+System.out.println("Post jira4 branch change develop");
+System.out.println("Post jira4 branch change develop gitx");
+System.out.println("added new file gitx");
+		return res;
+
+		
+	}
+	
+	
+}
